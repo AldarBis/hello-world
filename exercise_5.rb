4aefc8b5f08ea6389c6b610e1ca312734d9816c9
AldarBis <33025891+AldarBis@users.noreply.github.com>
2017-12-06 13:51:26 +0300

    Add files via upload

---
+++ exercise_5.rb
@@ -0,0 +1,14 @@
+class Desert
+  attr_accessor :name
+  attr_accessor :calories
+  def initialize(name, calories)
+    @name = name
+    @calories = calories
+  end
+  def healthy?
+     @calories < 200
+  end
+  def delicious?
+    true
+  end
+end
