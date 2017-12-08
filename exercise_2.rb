e4f1a14d7f6ab883d5af28c5ea1feaa7d6738fa2
AldarBis <33025891+AldarBis@users.noreply.github.com>
2017-12-08 22:23:26 +0300

    Update exercise_2.rb

--- exercise_2.rb
+++ exercise_2.rb
@@ -1,16 +1,10 @@
 def count_words(string)
-  str = string.downcase.scan(/\w+/)
+  str = string.downcase.scan(/\w+/).sort
+  puts str
   hash = Hash.new(0)
-  str.each {|n|
-next if hash.has_key?(n)
-    str.each { |m|    
-      if n == m
-      hash[n] += 1
-      end
-       # puts hash
-      }
-    }
+  str.each { |n| hash[n] = str.count(n) }
   puts hash
 end
 
 count_words("Doo bee doo bee doo")
+count_words("A man, a plan, a canal -- Panama")
