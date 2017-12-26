c9790b98c7928224fb68f6d130154d88af339116
AldarBis <33025891+AldarBis@users.noreply.github.com>
2017-12-26 15:40:29 +0300

    Add files via upload

---
+++ exercise_9.rb
@@ -0,0 +1,30 @@
+
+
+class String 
+def palindrome? 
+
+str = self.downcase.delete("^a-z&&^а-я") 
+str_rev = str.reverse 
+if str == str_rev && !str.empty? 
+puts "palindrome" 
+else 
+puts "No palindrome" 
+end 
+end 
+end 
+
+module Enumerable 
+def palindrome? 
+
+str = self#.downcase.delete("^a-z&&^а-я") 
+str_rev = str.reverse 
+if str == str_rev && !str.empty? 
+puts "palindrome" 
+else 
+puts "No palindrome" 
+end 
+end 
+end 
+
+"foo".palindrome? 
+[1,2,3,2,1].palindrome?
