f61404db5e6c67443cd6eb658f43e30dcbdd774c
AldarBis <33025891+AldarBis@users.noreply.github.com>
2017-12-26 16:36:16 +0300

    Update exercise_4.rb

--- exercise_4.rb
+++ exercise_4.rb
@@ -1,23 +1,18 @@
 def combine_anagrams(words)
   res_hash = {}
+  res_array = []
   words.each do |word|
   word.downcase!
   key = word.split('').sort.join
   if res_hash.has_key?(key)
     res_hash[key].push(word)
   else
-  
     res_hash[key] = [word]
   end
-end
-
-
- res_array = []
-res_hash.each do |k, v|
-  res_array.push(v)
-  
-end
-p res_array
+  end
+  res_hash.each do |k, v|
+  res_array.push(v)  
+  end
 end
 
 words =  ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']
