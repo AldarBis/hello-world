8b030bbac4419c4566b04aba9577cfc175dba2d8
AldarBis <33025891+AldarBis@users.noreply.github.com>
2017-12-26 16:55:30 +0300

    Update exercise_9.rb

--- exercise_9.rb
+++ exercise_9.rb
@@ -1,29 +1,25 @@
-
-
 class String 
-def palindrome? 
-
-str = self.downcase.delete("^a-z&&^а-я") 
-str_rev = str.reverse 
-if str == str_rev && !str.empty? 
-puts "palindrome" 
-else 
-puts "No palindrome" 
-end 
-end 
+  def palindrome? 
+    str = self.downcase.delete("^a-z&&^а-я") 
+    str_rev = str.reverse 
+    if str == str_rev && !str.empty? 
+      puts "palindrome" 
+    else 
+      puts "No palindrome" 
+    end 
+  end 
 end 
 
 module Enumerable 
-def palindrome? 
-
-str = self#.downcase.delete("^a-z&&^а-я") 
-str_rev = str.reverse 
-if str == str_rev && !str.empty? 
-puts "palindrome" 
-else 
-puts "No palindrome" 
-end 
-end 
+  def palindrome? 
+    str = self 
+    str_rev = str.reverse 
+    if str == str_rev && !str.empty? 
+      puts "palindrome" 
+    else 
+      puts "No palindrome" 
+    end 
+  end 
 end 
 
 "foo".palindrome? 
