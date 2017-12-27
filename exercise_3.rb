809a0548fd942b2da39bfeb6be57c5e64c62531d
AldarBis <33025891+AldarBis@users.noreply.github.com>
2017-12-27 16:07:32 +0300

    Add files via upload

---
+++ exercise_3.rb
@@ -0,0 +1,57 @@
+class WrongNumberOfPlayersError < StandardError 
+  def message 
+    "Wrong number of players" 
+  end 
+end 
+
+class NotSuchStrategyError < StandardError 
+  def message 
+    "Not such strategy error" 
+  end 
+end 
+
+def rps_game_winner(array) 
+  if array.count > 2 
+    raise WrongNumberOfPlayersError 
+  end 
+  
+  array.collect { |state|  
+    unless state[1] == "R" || state[1] == "P" || state[1] == "S"
+      raise NotSuchStrategyError
+      next
+    end
+  }
+  
+  case array[0][1]
+    when "R"
+	  case array[1][1]
+	    when "R"
+		  print array[0]
+	    when "P"
+		  print array[1]
+	    when "S"
+		  print array[0]
+	  end
+	when "P"
+	  case array[1][1]
+	    when "R"
+		  print array[0]
+	    when "P"
+		  print array[0]
+	    when "S"
+		  print array[1]
+	  end
+	when "S"
+	  case array[1][1]
+	    when "R"
+		  print array[1]
+	    when "P"
+		  print array[0]
+	    when "S"
+		  print array[0]
+	  end
+	end
+  end
+ 
+arr = [["Armando", "P"],["Dave", "S"]] 
+rps_game_winner(arr)
