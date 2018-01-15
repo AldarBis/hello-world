4ec58191d3edeb28ee4d32edb4cec1ede35310aa
AldarBis <33025891+AldarBis@users.noreply.github.com>
2018-01-15 08:30:30 +0300

    Update exercise_7.rb

--- exercise_7.rb
+++ exercise_7.rb
@@ -1,38 +1,46 @@
 module Currency
+  puts "Dol currency"
+  $dol = gets.chomp.to_f
+  puts "Eur currency"
+  $eur = gets.chomp.to_f
   def dollar
-    array = [0.74,32.26,1, self]
+   array = [$dol/$eur, $dol, 1, self]
   end
-  alias :dollars :dollar
+	alias :dollars :dollar
 	
   def euro
-    array = [1,43.61,1.35, self]
+   array = [1, $eur, $eur/$dol, self]
   end
-  alias :euros :euro
+    alias :euros :euro
   
   def ruble
-    array = [0.23,1,0.03, self]
+    array = [1/$eur, 1, 1/$dol, self]
+	
   end
   alias :rubles :ruble
   
   def in(curr)
     case curr
-    when :dollar, :dollars
-      puts self[2]*self[3] 
-    when :euro, :euros
-      puts self[0]*self[3]
-    when :ruble, :rubles
-      puts self[1]*self[3]
+	   when :dollar, :dollars
+	    puts (self[2]*self[3]).round 3
+	   when :euro, :euros
+	    puts (self[0]*self[3]).round 3
+	   when :ruble, :rubles
+	    puts (self[1]*self[3]).round 3
+	  	
     end
-  end
+end
 end
 
-class Fixnum
-  include Currency
-end
 
+class Integer
+ include Currency
+end
 class Array
-  include Currency
+include Currency
 end
+
+
 
 5.dollars.in(:euros)
 10.euros.in(:rubles)
