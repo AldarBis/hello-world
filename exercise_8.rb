8a33aeb583bb633ac55a1f382e81dd1bbee2482b
AldarBis <33025891+AldarBis@users.noreply.github.com>
2018-01-15 08:31:33 +0300

    Update exercise_8.rb

--- exercise_8.rb
+++ exercise_8.rb
@@ -1,27 +1,25 @@
 class Class 
   def attr_accessor_with_history(attr_name) 
     attr_reader attr_name
-    class_eval %Q{ 
       def self.my_attr_writer(attr_name) 
         @@arr = [nil]
         check = 0
         flag = false
         define_method("#{attr_name}=") do |new_value|   
-	  if self != check && flag
-	  @@arr = [nil]
-	  flag = false
-	  else
-	  check = self
-	  flag = true
-	  end
+	      if self != check && flag
+	        @@arr = [nil]
+	        flag = false
+	      else
+	        check = self
+	        flag = true
+	      end
           instance_variable_set("@#{attr_name}", new_value) 
           @@arr.push(instance_variable_get("@#{attr_name}")) 
         end 
       end 
-      def bar_history 
+      define_method (:bar_history) do
         print @@arr 
-      end 
-    }  
+      end  
     my_attr_writer attr_name
   end
 end 
@@ -42,4 +40,3 @@ g.bar = 31
 g.bar = 2 
 g.bar = 'Caramba!'
 g.bar_history
-
